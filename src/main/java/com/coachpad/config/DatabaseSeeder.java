ccf1f6139c4677ed2b36f33c7314ffff1f1a6936
sahar <hammamisahar456@gmail.com>
2026-04-03 16:15:45 +0100

    database

--- src/main/java/com/coachpad/config/DatabaseSeeder.java
+++ src/main/java/com/coachpad/config/DatabaseSeeder.java
@@ -40,42 +40,48 @@ public class DatabaseSeeder {
                 .logoFilePath("https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Real_Madrid_CF.svg/1200px-Real_Madrid_CF.svg.png")
                 .usePlayerPhotos(true).build();
         TeamEntity team = TeamEntity.builder()
-                .name("Real Madrid").nickname("Los Blancos").ageCategory("Senior").design(design).build();
+                .name("Real Madrid").nickname("Los Blancos").ageCategory("SENIOR").design(design).build();
         colors.setDesign(design); design.setTeam(team);
 
         // Staff Technique
         team.addCoach(createCoach("Carlo", "Ancelotti", CoachRole.HEAD_COACH, LicenseLevel.UEFA_PRO, CoachingPhilosophy.TACTICAL_FLEXIBILITY, "https://b.fssta.com/uploads/application/soccer/headshots/2107.png"));
         team.addCoach(createCoach("Davide", "Ancelotti", CoachRole.ASSISTANT_COACH, LicenseLevel.UEFA_PRO, CoachingPhilosophy.TACTICAL_FLEXIBILITY, ""));
-        team.addCoach(createCoach("Francesco", "Mauri", CoachRole.ASSISTANT_COACH, LicenseLevel.UEFA_PRO, CoachingPhilosophy.BALANCED, ""));
-        team.addCoach(createCoach("Luis", "Llopis", CoachRole.GOALKEEPER_COACH, LicenseLevel.UEFA_PRO, CoachingPhilosophy.BALANCED, ""));
         team.addCoach(createCoach("Antonio", "Pintus", CoachRole.FITNESS_COACH, LicenseLevel.NONE, CoachingPhilosophy.BALANCED, ""));
-        
-        // Staff Médical
-        team.addMedicalStaff(createCoach("Niko", "Mihic", CoachRole.DOCTOR, LicenseLevel.NONE, CoachingPhilosophy.BALANCED, ""));
-        team.addMedicalStaff(createCoach("Joaquín", "Mas", CoachRole.DOCTOR, LicenseLevel.NONE, CoachingPhilosophy.BALANCED, ""));
 
-        // Players (Complet)
+        // Gardiens
         team.addPlayer(createPlayer("Thibaut", "Courtois", 1, "GK", "Belgium", "1992-05-11", "https://b.fssta.com/uploads/application/soccer/headshots/711.png"));
-        team.addPlayer(createPlayer("Andriy", "Lunin", 13, "GK", "Ukraine", "1999-02-11", "https://b.fssta.com/uploads/application/soccer/headshots/45404.png"));
+        team.addPlayer(createPlayer("Andriy", "Lunin", 13, "GK", "Ukraine", "1999-02-11", "https://b.fssta.com/uploads/application/soccer/headshots/53635.png"));
+        team.addPlayer(createPlayer("Fran", "González", 25, "GK", "Spain", "2005-06-24", "https://b.fssta.com/uploads/application/soccer/headshots/101037.png"));
+
+        // Défenseurs
         team.addPlayer(createPlayer("Dani", "Carvajal", 2, "RB", "Spain", "1992-01-11", "https://b.fssta.com/uploads/application/soccer/headshots/2029.png"));
         team.addPlayer(createPlayer("Éder", "Militão", 3, "CB", "Brazil", "1998-01-18", "https://b.fssta.com/uploads/application/soccer/headshots/45402.png"));
         team.addPlayer(createPlayer("David", "Alaba", 4, "CB", "Austria", "1992-06-24", "https://b.fssta.com/uploads/application/soccer/headshots/2022.png"));
-        team.addPlayer(createPlayer("Antonio", "Rüdiger", 22, "CB", "Germany", "1993-03-03", "https://b.fssta.com/uploads/application/soccer/headshots/14294.png"));
-        team.addPlayer(createPlayer("Ferland", "Mendy", 23, "LB", "France", "1995-06-08", "https://b.fssta.com/uploads/application/soccer/headshots/45412.png"));
-        team.addPlayer(createPlayer("Federico", "Valverde", 8, "CM", "Uruguay", "1998-07-22", "https://b.fssta.com/uploads/application/soccer/headshots/40620.png"));
-        team.addPlayer(createPlayer("Aurélien", "Tchouaméni", 14, "CDM", "France", "2000-01-27", "https://b.fssta.com/uploads/application/soccer/headshots/56961.png"));
-        team.addPlayer(createPlayer("Eduardo", "Camavinga", 6, "CM", "France", "2002-11-10", "https://b.fssta.com/uploads/application/soccer/headshots/65301.png"));
+        team.addPlayer(createPlayer("Antonio", "Rüdiger", 22, "CB", "Germany", "1993-03-03", "https://b.fssta.com/uploads/application/soccer/headshots/2330.png"));
+        team.addPlayer(createPlayer("Ferland", "Mendy", 23, "LB", "France", "1995-06-08", "https://b.fssta.com/uploads/application/soccer/headshots/54266.png"));
+        team.addPlayer(createPlayer("Lucas", "Vázquez", 17, "RB", "Spain", "1991-07-01", "https://b.fssta.com/uploads/application/soccer/headshots/2030.png"));
+        team.addPlayer(createPlayer("Fran", "García", 20, "LB", "Spain", "1999-08-14", "https://b.fssta.com/uploads/application/soccer/headshots/60109.png"));
+        team.addPlayer(createPlayer("Jesús", "Vallejo", 18, "CB", "Spain", "1997-01-05", "https://b.fssta.com/uploads/application/soccer/headshots/2021.png"));
+
+        // Milieux
         team.addPlayer(createPlayer("Jude", "Bellingham", 5, "CAM", "England", "2003-06-29", "https://b.fssta.com/uploads/application/soccer/headshots/71310.png"));
-        team.addPlayer(createPlayer("Luka", "Modric", 10, "CM", "Croatia", "1985-09-09", "https://b.fssta.com/uploads/application/soccer/headshots/2027.png"));
-        team.addPlayer(createPlayer("Vinícius", "Júnior", 7, "LW", "Brazil", "2000-07-12", "https://b.fssta.com/uploads/application/soccer/headshots/40632.png"));
-        team.addPlayer(createPlayer("Rodrygo", "Goes", 11, "RW", "Brazil", "2001-01-09", "https://b.fssta.com/uploads/application/soccer/headshots/47913.png"));
-        team.addPlayer(createPlayer("Kylian", "Mbappé", 9, "ST", "France", "1998-12-20", "https://b.fssta.com/uploads/application/soccer/headshots/45415.png"));
-        team.addPlayer(createPlayer("Brahim", "Díaz", 21, "RW", "Morocco", "1999-08-03", "https://b.fssta.com/uploads/application/soccer/headshots/47201.png"));
-        team.addPlayer(createPlayer("Arda", "Güler", 15, "AM", "Turkey", "2005-02-25", "https://b.fssta.com/uploads/application/soccer/headshots/92745.png"));
+        team.addPlayer(createPlayer("Eduardo", "Camavinga", 6, "CM", "France", "2002-11-10", "https://b.fssta.com/uploads/application/soccer/headshots/69367.png"));
+        team.addPlayer(createPlayer("Federico", "Valverde", 8, "CM", "Uruguay", "1998-07-22", "https://b.fssta.com/uploads/application/soccer/headshots/42653.png"));
+        team.addPlayer(createPlayer("Luka", "Modric", 10, "CM", "Croatia", "1985-09-09", "https://b.fssta.com/uploads/application/soccer/headshots/880.png"));
+        team.addPlayer(createPlayer("Aurélien", "Tchouaméni", 14, "CDM", "France", "2000-01-27", "https://b.fssta.com/uploads/application/soccer/headshots/58145.png"));
+        team.addPlayer(createPlayer("Arda", "Güler", 15, "AM", "Turkey", "2005-02-25", "https://b.fssta.com/uploads/application/soccer/headshots/90609.png"));
+        team.addPlayer(createPlayer("Dani", "Ceballos", 19, "CM", "Spain", "1996-08-07", "https://b.fssta.com/uploads/application/soccer/headshots/31338.png"));
+
+        // Attaquants
+        team.addPlayer(createPlayer("Vinícius", "Júnior", 7, "LW", "Brazil", "2000-07-12", "https://b.fssta.com/uploads/application/soccer/headshots/53004.png"));
+        team.addPlayer(createPlayer("Kylian", "Mbappé", 9, "ST", "France", "1998-12-20", "https://b.fssta.com/uploads/application/soccer/headshots/40670.png"));
+        team.addPlayer(createPlayer("Rodrygo", "Goes", 11, "RW", "Brazil", "2001-01-09", "https://b.fssta.com/uploads/application/soccer/headshots/67951.png"));
         team.addPlayer(createPlayer("Endrick", "Felipe", 16, "ST", "Brazil", "2006-07-21", "https://b.fssta.com/uploads/application/soccer/headshots/113245.png"));
+        team.addPlayer(createPlayer("Brahim", "Díaz", 21, "RW", "Morocco", "1999-08-03", "https://b.fssta.com/uploads/application/soccer/headshots/41810.png"));
 
         return team;
     }
+
 
     private TeamEntity createRealMadridCastilla() {
         TeamKitColorsEntity colors = TeamKitColorsEntity.builder().primaryHex("#FFFFFF").secondaryHex("#000080").build();
@@ -111,41 +117,46 @@ public class DatabaseSeeder {
         TeamKitColorsEntity colors = TeamKitColorsEntity.builder().primaryHex("#004170").secondaryHex("#DA291C").trimHex("#FFFFFF").build();
         TeamDesignEntity design = TeamDesignEntity.builder().style(WidgetAppearance.CIRCLE).jerseyDesign(JerseyDesign.STRIPED_VERTICAL).colors(colors)
                 .logoFilePath("https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Paris_Saint-Germain_F.C..svg/1200px-Paris_Saint-Germain_F.C..svg.png").usePlayerPhotos(true).build();
-        TeamEntity team = TeamEntity.builder().name("Paris Saint-Germain").nickname("PSG").ageCategory("Senior").design(design).build();
+        TeamEntity team = TeamEntity.builder().name("Paris Saint-Germain").nickname("PSG").ageCategory("SENIOR").design(design).build();
         colors.setDesign(design); design.setTeam(team);
 
         // Staff Technique
         team.addCoach(createCoach("Luis", "Enrique", CoachRole.HEAD_COACH, LicenseLevel.UEFA_PRO, CoachingPhilosophy.TIKI_TAKA, "https://b.fssta.com/uploads/application/soccer/headshots/2357.png"));
         team.addCoach(createCoach("Rafel", "Pol", CoachRole.ASSISTANT_COACH, LicenseLevel.UEFA_PRO, CoachingPhilosophy.POSSESSION, ""));
-        team.addCoach(createCoach("Aitor", "Unzué", CoachRole.ANALYST, LicenseLevel.UEFA_A, CoachingPhilosophy.TIKI_TAKA, ""));
-        team.addCoach(createCoach("Borja", "Álvarez", CoachRole.GOALKEEPER_COACH, LicenseLevel.UEFA_PRO, CoachingPhilosophy.BALANCED, ""));
 
-        // Staff Médical
-        team.addMedicalStaff(createCoach("Christophe", "Baudot", CoachRole.DOCTOR, LicenseLevel.NONE, CoachingPhilosophy.BALANCED, ""));
-        team.addMedicalStaff(createCoach("Quincy", "Cyriel", CoachRole.PHYSIOTHERAPIST, LicenseLevel.NONE, CoachingPhilosophy.BALANCED, ""));
-
-        // Players (Complet)
-        team.addPlayer(createPlayer("Gianluigi", "Donnarumma", 1, "GK", "Italy", "1999-02-25", "https://b.fssta.com/uploads/application/soccer/headshots/31411.png"));
+        // Gardiens
+        team.addPlayer(createPlayer("Gianluigi", "Donnarumma", 1, "GK", "Italy", "1999-02-25", "https://b.fssta.com/uploads/application/soccer/headshots/31875.png"));
         team.addPlayer(createPlayer("Matvey", "Safonov", 39, "GK", "Russia", "1999-02-25", "https://b.fssta.com/uploads/application/soccer/headshots/65305.png"));
-        team.addPlayer(createPlayer("Achraf", "Hakimi", 2, "RB", "Morocco", "1998-11-04", "https://b.fssta.com/uploads/application/soccer/headshots/40602.png"));
-        team.addPlayer(createPlayer("Marquinhos", "Aoás Corrêa", 5, "CB", "Brazil", "1994-05-14", "https://b.fssta.com/uploads/application/soccer/headshots/17278.png"));
-        team.addPlayer(createPlayer("Lucas", "Hernández", 21, "CB", "France", "1996-02-14", "https://b.fssta.com/uploads/application/soccer/headshots/31345.png"));
+        team.addPlayer(createPlayer("Arnau", "Tenas", 80, "GK", "Spain", "2001-05-30", "https://b.fssta.com/uploads/application/soccer/headshots/113458.png"));
+
+        // Défenseurs
+        team.addPlayer(createPlayer("Achraf", "Hakimi", 2, "RB", "Morocco", "1998-11-04", "https://b.fssta.com/uploads/application/soccer/headshots/44920.png"));
+        team.addPlayer(createPlayer("Marquinhos", "Aoás Corrêa", 5, "CB", "Brazil", "1994-05-14", "https://b.fssta.com/uploads/application/soccer/headshots/75573.png"));
+        team.addPlayer(createPlayer("Lucas", "Hernández", 21, "CB", "France", "1996-02-14", "https://b.fssta.com/uploads/application/soccer/headshots/1104.png"));
         team.addPlayer(createPlayer("Willian", "Pacho", 51, "CB", "Ecuador", "2001-10-16", "https://b.fssta.com/uploads/application/soccer/headshots/113456.png"));
-        team.addPlayer(createPlayer("Lucas", "Beraldo", 35, "CB", "Brazil", "2003-11-24", "https://b.fssta.com/uploads/application/soccer/headshots/113457.png"));
-        team.addPlayer(createPlayer("Nuno", "Mendes", 25, "LB", "Portugal", "2002-06-19", "https://b.fssta.com/uploads/application/soccer/headshots/74620.png"));
+        team.addPlayer(createPlayer("Lucas", "Beraldo", 35, "CB", "Brazil", "2003-11-24", "https://b.fssta.com/uploads/application/soccer/headshots/90475.png"));
+        team.addPlayer(createPlayer("Nuno", "Mendes", 25, "LB", "Portugal", "2002-06-19", "https://b.fssta.com/uploads/application/soccer/headshots/74576.png"));
+        team.addPlayer(createPlayer("Milan", "Škriniar", 37, "CB", "Slovakia", "1995-02-11", "https://b.fssta.com/uploads/application/soccer/headshots/42188.png"));
+        team.addPlayer(createPlayer("Yoram", "Zague", 42, "RB", "France", "2006-05-15", "https://b.fssta.com/uploads/application/soccer/headshots/113459.png"));
+
+        // Milieux
         team.addPlayer(createPlayer("Vitinha", "Machado", 17, "CM", "Portugal", "2000-02-13", "https://b.fssta.com/uploads/application/soccer/headshots/68735.png"));
-        team.addPlayer(createPlayer("Warren", "Zaïre-Emery", 33, "CM", "France", "2006-03-08", "https://b.fssta.com/uploads/application/soccer/headshots/101036.png"));
-        team.addPlayer(createPlayer("Fabián", "Ruiz", 8, "CM", "Spain", "1996-04-03", "https://b.fssta.com/uploads/application/soccer/headshots/45422.png"));
+        team.addPlayer(createPlayer("Warren", "Zaïre-Emery", 33, "CM", "France", "2006-03-08", "https://b.fssta.com/uploads/application/soccer/headshots/106915.png"));
+        team.addPlayer(createPlayer("Fabián", "Ruiz", 8, "CM", "Spain", "1996-04-03", "https://b.fssta.com/uploads/application/soccer/headshots/30456.png"));
         team.addPlayer(createPlayer("João", "Neves", 87, "CM", "Portugal", "2004-09-27", "https://b.fssta.com/uploads/application/soccer/headshots/115678.png"));
-        team.addPlayer(createPlayer("Ousmane", "Dembélé", 10, "RW", "France", "1997-05-15", "https://b.fssta.com/uploads/application/soccer/headshots/37456.png"));
-        team.addPlayer(createPlayer("Bradley", "Barcola", 29, "LW", "France", "2002-09-02", "https://b.fssta.com/uploads/application/soccer/headshots/92744.png"));
-        team.addPlayer(createPlayer("Marco", "Asensio", 11, "CAM", "Spain", "1996-01-21", "https://b.fssta.com/uploads/application/soccer/headshots/31346.png"));
+        team.addPlayer(createPlayer("Senny", "Mayulu", 24, "CAM", "France", "2006-05-17", "https://b.fssta.com/uploads/application/soccer/headshots/113460.png"));
+
+        // Attaquants
+        team.addPlayer(createPlayer("Ousmane", "Dembélé", 10, "RW", "France", "1997-05-15", "https://b.fssta.com/uploads/application/soccer/headshots/39732.png"));
+        team.addPlayer(createPlayer("Bradley", "Barcola", 29, "LW", "France", "2002-09-02", "https://b.fssta.com/uploads/application/soccer/headshots/95034.png"));
+        team.addPlayer(createPlayer("Marco", "Asensio", 11, "CAM", "Spain", "1996-01-21", "https://b.fssta.com/uploads/application/soccer/headshots/7199.png"));
         team.addPlayer(createPlayer("Lee", "Kang-in", 19, "RW", "South Korea", "2001-02-19", "https://b.fssta.com/uploads/application/soccer/headshots/60601.png"));
-        team.addPlayer(createPlayer("Gonçalo", "Ramos", 9, "ST", "Portugal", "2001-06-20", "https://b.fssta.com/uploads/application/soccer/headshots/68737.png"));
-        team.addPlayer(createPlayer("Randal", "Kolo Muani", 23, "ST", "France", "1998-12-05", "https://b.fssta.com/uploads/application/soccer/headshots/65303.png"));
+        team.addPlayer(createPlayer("Gonçalo", "Ramos", 9, "ST", "Portugal", "2001-06-20", "https://b.fssta.com/uploads/application/soccer/headshots/74559.png"));
+        team.addPlayer(createPlayer("Randal", "Kolo Muani", 23, "ST", "France", "1998-12-05", "https://b.fssta.com/uploads/application/soccer/headshots/50949.png"));
 
         return team;
     }
+
 
     private TeamEntity createPSGU19() {
         TeamKitColorsEntity colors = TeamKitColorsEntity.builder().primaryHex("#004170").secondaryHex("#DA291C").build();
